e1bc7d7cabd3690b6aa33a8bcf09a1ded1689dd4
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2022-12-27 01:23:53 +0200

    Add: rotation of meteor

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -4,12 +4,17 @@ public class MainCamera : MonoBehaviour
 {
     public static float MinY { get; private set; }
     public static float MaxY { get; private set; }
+    public static float MinX { get; private set; }
+    public static float MaxX { get; private set; }
 
-    public float minY, maxY;
+    public float minY, maxY, minX, maxX;
 
     void Start()
     {
         MinY = minY;
         MaxY = maxY;
+
+        MinX = minX;
+        MaxX = maxX;
     }
 }
--- Assets/Scripts/MeteorSpawner.cs
+++ Assets/Scripts/MeteorSpawner.cs
@@ -5,7 +5,14 @@ public class MeteorSpawner : MonoBehaviour
     public GameObject meteorPrefab;
     public float timeToSpan;
 
+
     private float timer;
+    float deflectionAngle { get => Mathf.Atan2(MainCamera.MaxY, 2 * MainCamera.MaxX) / Mathf.PI * 180; }
+
+    void Start()
+    {
+
+    }
 
     void Update()
     {
@@ -15,9 +22,11 @@ public class MeteorSpawner : MonoBehaviour
         }
         else
         {
-            float rand = Random.Range(MainCamera.MinY, MainCamera.MaxY);
+            float randY = Random.Range(MainCamera.MinY, MainCamera.MaxY);
+            float randAngle = Random.Range(-deflectionAngle, deflectionAngle);
 
-            GameObject meteor = Instantiate(meteorPrefab, transform.position + Vector3.up * rand, new Quaternion(0, 180, 0, 0));
+            GameObject meteor = Instantiate(meteorPrefab, transform.position + Vector3.up * randY, transform.rotation);
+            meteor.transform.Rotate(0, 0, 180 + randAngle);
 
             timer = timeToSpan;
         }
